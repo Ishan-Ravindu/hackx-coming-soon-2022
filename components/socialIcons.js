10b540cb7cf3aa9a9d4ab4d9bdd400996649db4c
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-07 19:16:17 +0530

    remove rel in a tag

--- components/socialIcons.js
+++ components/socialIcons.js
@@ -4,13 +4,13 @@ function SocialIcons() {
     return (
         <div className={styles.social}>
             <ul>
-                <li><a href="https://www.facebook.com/imhackx/" rel="noopener" target={"_blank"}><span>Facebook</span> <i className="fa fa-facebook"></i></a></li>
-                <li><a href="https://www.instagram.com/hackx_uok/?hl=en" rel="noopener" target={"_blank"}><span>Instagram</span> <i className="fa fa-instagram"></i></a></li>
-                <li><a href="https://twitter.com/hackx_uok" rel="noopener" target={"_blank"}><span>Twitter</span> <i className="fa fa-twitter"></i></a></li>
+                <li><a href="https://www.facebook.com/imhackx/" ><span>Facebook</span> <i className="fa fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/hackx_uok/?hl=en" ><span>Instagram</span> <i className="fa fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/hackx_uok" ><span>Twitter</span> <i className="fa fa-twitter"></i></a></li>
                 {/* <li><a href="https://www.linkedin.com/in/teonamushambadze" target={"_blank"}><span>Linkedin</span> <i
                     className="fa fa-linkedin"></i></a>
                 </li> */}
-                <li><a href="https://www.youtube.com/c/hackXUoK/" rel="noopener" target={"_blank"}><span>Youtube</span> <i className="fa fa-youtube"></i></a></li>
+                <li><a href="https://www.youtube.com/c/hackXUoK/" ><span>Youtube</span> <i className="fa fa-youtube"></i></a></li>
             </ul>
         </div>
     );
