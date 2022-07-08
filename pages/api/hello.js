eba977489c30e8da0c057f5d5a53856411aa1e5f
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-08 11:02:41 +0530

    added endpoit to add subscribers

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,26 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const sdk = require('api')('@mailerliteapi/v2#3vxhg2kkw1s00g2');
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+async function postSubscriber(email) {
+  const options = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'X-MailerLite-ApiDocs': 'true',
+      'Content-Type': 'application/json',
+      'X-MailerLite-ApiKey': process.env.X_Mailer_Lite_ApiKey
+    },
+    body: JSON.stringify({ email: email })
+  };
+  try {
+    const response = await fetch('https://api.mailerlite.com/api/v2/subscribers', options);
+    return await response.json()
+  } catch (error) {
+    return await error.json()
+  }
+}
+
+export default async function handler(req, res) {
+  const responce = await postSubscriber(req.body.email)
+  res.status(200).json(responce)
 }
