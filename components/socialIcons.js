4a7ab4b07abe70f36a5a6a1d5af497d40afb524c
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-10 20:41:36 +0530

    added more responsive to avator logo

--- components/socialIcons.js
+++ components/socialIcons.js
@@ -3,7 +3,7 @@ import styles from './socialIcons.module.css'
 import { SocialIcon } from 'react-social-icons';
 function SocialIcons() {
     return (
-        <div className='absolute bottom-0 right-0 m-4 flex space-x-2'>
+        <div className='fixed bottom-0 right-0 m-4 flex space-x-2'>
             <SocialIcon url="https://www.facebook.com/imhackx/" />
             <SocialIcon url="https://www.instagram.com/hackx_uok/?hl=en" />
             <SocialIcon url="https://twitter.com/hackx_uok" />
--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
       <img className={styles.animalImg} src="hi stickker.png" alt="animal" />
 
       {/* <AppModel /> */}
-      <SocialIcon />
+
 
       <div className='container mx-auto min-h-screen  grid justify-items-center items-center'>
         <div className="space-y-8 px-4">
@@ -49,69 +49,9 @@ export default function Home() {
 
 
         </div>
-
+        <SocialIcon />
       </div >
     </>
   )
 }
 
-// export default function Home() {
-//   return (
-//     <>
-//       <AppModel />
-//       <SocialIcon />
-//       <div className={styles.animalContainer}>
-//         <Image
-//           className={styles.animalImg}
-//           alt="background"
-//           src={animal}
-//           layout="intrinsic"
-//           width={383}
-//           height={476}
-//           quality={100}
-//           priority
-//         />
-//       </div>
-
-//       <img class="object-none object-left-bottom bg-yellow-300 w-24 h-24" src="hi stickker.png" alt={animal} />
-//       <div className={styles.bgWrap}>
-//         <Image
-//           alt="background"
-//           src={background}
-//           layout="fill"
-//           objectFit="cover"
-//           quality={100}
-//           priority
-//         />
-//       </div>
-
-//       <Container fixed>
-//         <Stack className={styles.stack}>
-//           <dv></dv>
-//           <div>
-//             <Image
-//               alt="background"
-//               src={logo}
-//               // className={styles.image}
-//               layout="intrinsic"
-//               width={450}
-//               height={120}
-//               priority
-//             />
-//           </div>
-
-//           <div>
-//             <CountDown />
-//           </div>
-//           <div className={styles.footer} >
-//             <div className={styles.comingSoon}>
-//               <b>COMING</b> SOON !
-//             </div>
-//             <div className={styles.description}>THE MOST AWAITED INTER - UNIVERSITY STARTUP CHALLENGE</div>
-//           </div>
-//           <div></div>
-//         </Stack>
-//       </Container>
-//     </>
-//   )
-// }
