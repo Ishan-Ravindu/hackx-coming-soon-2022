864ea02687801470b75d8f39433714cab93a47d2
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-11 12:11:33 +0530

    changed domai to hackx.lk

--- components/AppModel.js
+++ components/AppModel.js
@@ -24,7 +24,7 @@ export default function AppModel() {
             if (!open) {
                 setOpen(true)
             }
-        }, 10 * 1000);
+        }, 3 * 1000);
 
         return () => {
             clearTimeout(timer);
@@ -54,7 +54,7 @@ export default function AppModel() {
         if (isEmail) {
             setLoading(true)
             try {
-                const response = await fetch('https://hackx-coming-soon-2022.vercel.app/api/hello', options);
+                const response = await fetch('https://www.hackx.lk/api/hello', options);
                 setLoading(false)
                 setOpen(false)
                 console.log(response.status)
